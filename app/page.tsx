684825a7bfad1f0a2786c9fcd367d3e3bbf5eb23
Mohammad Frotan <m.frotan@gmail.com>
2023-11-12 01:22:07 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="h1-bold">Hi Frotan</h1>
+        <h2 className="h2-bold">Hi Frotan</h2>
+        <h1>Hi Frotan</h1>
+      </div>
+    </main>
+  );
+}
